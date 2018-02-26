eb49d0ab1fdf0fb93bb2d0c267a35bfd9d0fb091
Eduardo Thomé <eduardo@estudiosis.com.br>
2018-02-26 17:25:44 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Food.create(name:"Bife de Patinho",protein:0.4,carb:0,fat:0.1,kcal:2.2)
+Food.create(name:"Acém (moído)",protein:0.2,carb:0,fat:0,kcal:1.4)
+Food.create(name:"Frango (a milanesa)",protein:0.3,carb:0.1,fat:0,kcal:2.2)
+Food.create(name:"Arroz Cozido",protein:0,carb:0.3,fat:0,kcal:1.3
+Food.create(name:"Lata de Atum",protein:0.15,carb:0,fat:0.02,kcal:0.7,
+Food.create(name:"Leite (Desnatado)",protein:0.03,carb:0.05,fat:0,kcal:0.32,
+Food.create(name:"Leite (Integral)",protein:0.03,carb:0.04,fat:0.03,kcal:0.57
+Food.create(name:"Aveia (flocos)",protein:0.14,carb:0.6,fat:0.08,kcal:3.94,
+Food.create(name:"Albumina Naturovos",protein:0.84,carb:0.05,fat:0,kcal:3.64,0
+Food.create(name:"Achocolatado (Nescau)",protein:0.19,carb:0.5,fat:0.12,kcal:2.15,
+Food.create(name:"Ovo cozido (unidade)",protein:"6",carb:"0",fat:"5",kcal:71,
